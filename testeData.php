ddef114e5491066ee63f2ac693bccdb9927adc0b
William Ferreira <jtcwilliam@gmail.com>
2025-08-11 13:55:44 -0300

    envio assinatura adaptada para IOS

--- testeData.php
+++ testeData.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<script>
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+ 
+
+</script>
+
+</body>
+</html>
+
+
 <?php
 
 
